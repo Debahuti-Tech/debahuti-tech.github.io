2dfd3e53ebe75d2ef748ac0816119dd2befa5ddc
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2025-11-03 02:37:18 +0530

    page.tsx : Disable pages pages which is not requried

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,14 +22,14 @@ export default function Home() {
     <>
       <ScrollUp />
       <Hero />
-      <Features />
-      <Video />
-      <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+      {/* <Features /> */}
+      {/* <Video />
+      <Brands /> */}
+      {/* <AboutSectionOne />
+      <AboutSectionTwo /> */}
       <Testimonials />
-      <Pricing />
-      <Blog />
+      {/* <Pricing />
+      <Blog /> */}
       <Contact />
     </>
   );
